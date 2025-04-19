0269c01542dcbf7d57a4e93e4c291a7276a868de
anand-kamble <amk23j@fsu.edu>
2025-04-19 19:22:09 -0400

    [apis]: Now python backend can serve the static frontend as well.
    You are required to build to the frontend using vite before starting the server

--- apis/app.py
+++ apis/app.py
@@ -1,24 +1,32 @@
 from typing import Annotated
 from fastapi import Depends, FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 import uvicorn
-import importlib 
+import importlib
 import sys
-import os 
+import os
 from contextlib import asynccontextmanager
 from services.db import nc_db
+
 
 # Add the current script's directory to the system path
 sys.path.append(os.path.dirname(__file__))
 
+
 # Dynamically import the `routes` module and get all routers
 router_module = importlib.import_module(".", package="routes")  # Import module
-routers = [getattr(router_module, var) for var in dir(router_module) if not var.startswith("__")]
+routers = [
+    getattr(router_module, var)
+    for var in dir(router_module)
+    if not var.startswith("__")
+]
 routers = [router for router in routers if isinstance(router, APIRouter)]
 
 
-
 # Create an instance of the database
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -39,13 +47,28 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+dist_path = os.path.join(os.path.dirname(__file__), "../web/dist/")
+app.mount(
+    "/assets", StaticFiles(directory=os.path.join(dist_path, "assets")), name="assets"
+)
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello Worrrrld"}
+async def serve_index():
+    return FileResponse(os.path.join(dist_path, "index.html"))
+
 
 # Register all dynamically loaded routers
 for router in routers:
-    app.include_router(router)
+    app.include_router(router, prefix="/api")
+    
+@app.get("/{full_path:path}")
+def serve_spa(full_path: str):
+    target_file = os.path.join(dist_path, full_path)
+    if not full_path.startswith("api") and os.path.isfile(os.path.join(dist_path, full_path)):
+        return FileResponse(os.path.join(dist_path, full_path))
+    return FileResponse(os.path.join(dist_path, "index.html"))
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=3030)
